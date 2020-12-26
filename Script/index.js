d3e90c6490395b7f3ad2c6523da586040e9eebda
Justin <60296568+ComradeDiamond@users.noreply.github.com>
2020-12-26 18:45:50 -0500

    Customization Selection + Modal Creation
    - Created modal system to display customization options in
    - Created fadeBlack and zoom animations to accompany modal systems
    - Created reusable modal templates
    - CSS animations would now toggle modal display/appearance
    
    - Added customization JSON to keep track of customizations that will be in the end
    - Added parallel customization array to redirect all operations back to the appropriate JSON for the sake of reusing functions
    - FlexCard class now stored in a flexArr(?) array to keep track of
    - Clicking on flexCard class will now trigger modal to appear
    - Submitting the modal will remove all normal animations and store the info in main JSON
    
    - Create wrap element and overflow-y:hidden; to disable scroll movement while modal is on

--- Script/index.js
+++ Script/index.js
@@ -1,419 +1,97 @@
-const mailToJustin = 'mailto:justinchen3946@gmail.com?Subject=Email%20Justin!';
-const gitJustin = 'https://github.com/ComradeDiamond';
+customizations = {
+	presets: "none",
+	font: "Times New Roman",
+	cardSize: "12pt",
+	highlightColor: "None",
+	linkColor: "Black",
+	linkCustom: "None",
+	descriptionColor: "Black",
+	descriptionPosition: "None",
+	tagColor: "Black",
+	tagAlign: "Left",
+	tagSize: "12pt",
+	tagCustomization: "None"
+};
 
-const initialize = () => {
-	//Insert display
-	displayJSON = {
-	}
+//We'll use this + JSON bracket notation to later modify these things
+//hmm looks like it's going to be impossible to do functional programming unless we want 
+//to store everything in the frontend
+//Parallel array to the indexHTML order and customizations JSON
+const customizationNames = [
+	"presets", 
+	"font", 
+	"cardSize", 
+	"highlightColor", 
+	"linkColor", 
+	"linkCustom",
+	"descriptionColor",
+	"descriptionPosition",
+	"tagColor",
+	"tagAlign",
+	"tagSize",
+	"tagCustomization"
+];
+
+function initialize(document)
+{
+	custArr = Array.from(document.getElementsByClassName("flexCard"));
+	//Adds an event listener to each array and update all of their customizations
+	custArr.forEach((el, idx) => {
+		el.addEventListener("click", () => {
+			mdcDisplay(this, idx, document);
+		});
+		el.querySelector(".flexVal").innerText = customizations[customizationNames[idx]];
+	});
 }
 
-const getClipboard = (navigator) => new Promise((resolve, reject) => {
-	let clipboard = navigator.clipboard.readText();
+const mdcDisplay = (el, idx, document) => {
+	//Stops scrolling
+	document.body.style.overflowY = "hidden";
 
-	clipboard.then((text) => resolve(text));
-	clipboard.catch((err) => reject(""));
-});
+	//Inits the dom object. divItem is parallel to the corresponding custNames array
+	const tempWrap = document.getElementById("invisFormHTML");
+	const divItem = tempWrap.children[idx];
 
+	tempWrap.style.visibility = "visible";
+	divItem.style.display = "block";
 
-function seal(doIWipe) //Justin's blockfile presets #dank
-{
+	//Animation
+	tempWrap.classList.add("fadeBlackIn");
+	divItem.classList.add("zoomIn");
 
-	let tempLabel = document.createElement("div");
-	tempLabel.style.textAlign = "center";
-	
-	let tempSubLabel = document.createElement("span");
-	tempSubLabel.textContent = cardLabel.value;
-	tempSubLabel.style.fontFamily = "georgia"; //we could abstract these Georgia, but it's the same amt of work anyways
-	tempSubLabel.style.fontSize = "18pt";
-	tempSubLabel.style.backgroundColor = "#f0dbaa";
-	tempSubLabel.style.textDecoration = "underline";
-	tempSubLabel.style.fontWeight = "bold";
-	tempLabel.appendChild(tempSubLabel);
-
-	let tempAuthor = document.createElement("div");
-
-	let tempLink = document.createElement("a");
-	tempLink.href = cardLink.value
-	tempLink.target = "_blank";
-	tempLink.textContent = authorTag.value;
-	tempLink.style.fontFamily = "georgia";
-	tempLink.style.color = "#00a2ff";
-	tempLink.style.fontSize = "12pt";
-	tempLink.style.fontWeight = "bold";
-	tempLink.style.textDecoration = "none";
-	tempAuthor.appendChild(tempLink);
-
-	let tempDescription = document.createElement("div");
-	tempDescription.textContent = cardDescription.value;
-	tempDescription.style.fontFamily = "georgia"
-	tempDescription.style.color = "green";
-	tempDescription.style.fontStyle = "italic";
-	tempDescription.style.fontSize = "12pt";
-
-	let tempPaste = document.createElement("div");
-	tempPaste.innerText = inputForm.pasteBox.value;
-	tempPaste.style.fontFamily = "georgia";
-	tempPaste.style.fontSize = "12pt"
-	
-	//Tempdescription goes after
-	styleForm.descriptionPosition.value = "after";
-
-	display(tempLabel, tempAuthor, tempDescription, tempPaste, doIWipe);
+	//Add btn action - loose type saves lives :P
+	//The eAdded short circuiting prevents multiple event listeners from stacking
+	//!str == bool
+	!!divItem.eAdded || divItem.querySelector("img").addEventListener("click", () => {
+		mdcDisplaynt(document, tempWrap, divItem, idx);
+	})
 }
-function mla(doIWipe)
-{
-	let tempLabel = document.createElement("span");
-	tempLabel.innerText = cardLabel.value;
-	tempLabel.style.fontFamily = "Times New Roman"; //We could literally abstract all this but it's the same line length
-	tempLabel.style.fontSize = "14pt";
-	tempLabel.style.fontWeight = "bold";
-	tempLabel.style.textDecoration = "underline";
 
-	let tempAuthor = document.createElement("a");
-	tempAuthor.href = cardLink.value;
-	tempAuthor.style.fontFamily = "Times New Roman";
-	tempAuthor.style.fontSize = "12pt";
-	tempAuthor.style.fontWeight = "bold";
-	tempAuthor.innerText = authorTag.value;
+//If regularn't means former regular
+//Staffn't means former staff
+//Displayn't is next lmao
+const mdcDisplaynt = (document, tempWrap, divItem, idx) => {
 
-	let tempDescription = document.createElement("div");
-	tempDescription.innerText = cardDescription.value;
-	tempDescription.style.fontFamily = "Times New Roman";
-	tempDescription.style.fontSize = "12pt";
+	//Animations
+	tempWrap.classList.add("fadeBlackOut");
+	tempWrap.classList.remove("fadeBlackIn");
 
-	let tempPaste = document.createElement("div");
-	tempPaste.innerText = inputForm.pasteBox.value;
-	tempPaste.style.fontFamily = "Times New Roman";
-	tempPaste.style.fontSize = "12pt";
-	styleForm.descriptionPosition.value = "before";
+	divItem.classList.add("zoomOut");
+	divItem.classList.remove("zoomIn");
 
-	display(tempLabel, tempAuthor, tempDescription, tempPaste, doIWipe);
-}
-function bulletBoard(doIWipe)
-{
-	let tempList = document.createElement("ul");
+	let customizationVal = divItem.querySelector(".modalSelect").value;
 
-	let tempLabel = document.createElement("li");
-	//We're doing a no-no and making this innerHTML, but it doesn't matter because I'm lazy
-	//Yes I know making it innerHTML is very brainlet because code injection hacks are a thing
-	tempLabel.innerHTML = `${cardLabel.value} - <a href=${cardLink.value} style="text-decoration: none;" target="_blank">${authorTag.value}</a>`;
-	tempLabel.style.fontFamily = styleForm.fontFamily.value;
-	tempLabel.style.fontSize = "18pt";
-	tempList.appendChild(tempLabel);
+	customizations[customizationNames[idx]] = customizationVal;
+	custArr[idx].querySelector(".flexVal").innerText = customizationVal;
 
-	let tempPaste = document.createElement("li");
-	tempPaste.innerText = inputForm.pasteBox.value;
-	tempPaste.style.fontFamily = styleForm.fontFamily.value;
-	tempPaste.style.fontSize = "14pt";
-	tempPaste.style.marginLeft = "2%";
+	//Async clear elements and stuff after it ends
+	setTimeout(() => {
+		document.body.style.overflowY = "auto";
 
-	let tempDescription = document.createElement("li");
-	tempDescription.innerText = cardDescription.value;
-	tempDescription.style.fontFamily = styleForm.fontFamily.value;
-	tempDescription.style.fontSize = "14pt";
-	tempDescription.style.color = "green";
-	tempDescription.style.marginLeft = "2%";
+		divItem.classList.remove("zoomOut");
+		divItem.style.display = "none";
 
-	tempLabel.appendChild(tempDescription);
-	tempLabel.appendChild(tempPaste);
-
-	outputDiv.appendChild(tempList);
-
-	if (doIWipe)
-	{
-		authorTag.value = "";
-		cardLabel.value = "";
-		cardDescription.value = "";
-		cardLink.value = "";
-		inputForm.pasteBox.value = "";
-	}
-}
-function generate(doIWipe) //A clusterfuck of code - will try to comment
-{
-	let switchBoolean = false;
-	outputDiv.innerHTML = ""; //Resets outputDiv
-
-	/* Unused code - patched by changing textContent to innerText. Trade the span detection for newline detection.
-	Extra spacing is irrelevant because this is an article, so for the purposes of card cutting we're yeeting that out the window
-
-	pastedText = inputForm.pasteBox.value.replace(/ /ig, " ");
-	pastedText = pastedText.replace(/\n/ig, "\n");
-	console.log(pastedText);*/
-
-	//If there are certain presets, abide by those presets and return the rest
-	switch(styleForm.presets.value) 
-	{
-		case "seal":
-			seal(doIWipe);
-			switchBoolean = true;
-		break;
-
-		case "none":
-		break;
-
-		case "bulletBoard":
-			bulletBoard(doIWipe);
-			switchBoolean = true;
-		break;
-
-		case "mla":
-			mla(doIWipe);
-			switchBoolean = true;
-		break;
-
-		case "blockfile":
-			blockfile(doIWipe);
-			switchBoolean = true;
-		break;
-
-		default:
-			console.log("There is an error: please contact Justin to see what went wrong");
-			switchBoolean = true;
-	}
-
-	if (switchBoolean) //If the presets have been applied, just yeet the rest of the code.
-	{
-		return;
-	}
-
-	let tempLabel = document.createElement("span");
-	tempLabel.textContent = cardLabel.value;
-	tempLabel.style.fontFamily = styleForm.fontFamily.value;
-	if ((styleForm.labelSize.value <= 0) || (styleForm.labelSize.value > 48))
-	{
-		alert("Bruh stop trying to break the code this thing isn't made by Wix");
-		tempLabel.style.fontSize = "12pt";
-	}
-	else
-	{
-		tempLabel.style.fontSize = styleForm.labelSize.value + "pt";
-	}
-	tempLabel.fontFamily = styleForm.fontFamily;
-	if (styleForm.highlightEnter.value != "")
-	{
-		try
-		{
-			tempLabel.style.backgroundColor = styleForm.highlightEnter.value;
-		}
-		catch
-		{
-			alert("yea you don't know what you're doing");
-			tempLabel.style.backgroundColor = styleForm.labelHighlight.value;
-		}
-	}
-	else
-	{
-		tempLabel.style.backgroundColor = styleForm.labelHighlight.value;
-	}
-	tempLabel.appendChild(document.createElement("br"));
-
-	let tempAuthor = document.createElement("a");
-	tempAuthor.href = cardLink.value;
-	tempAuthor.target = "_blank";
-	tempAuthor.textDecoration = "none";
-	tempAuthor.textContent = authorTag.value;
-	tempAuthor.style.fontFamily = styleForm.fontFamily.value;
-	if (styleForm.authorTagEnter.value != "")
-	{
-		try
-		{
-			tempAuthor.style.color = styleForm.authorTagEnter.value;
-		}
-		catch
-		{
-			alert("yea you don't know what you're doing");
-			tempAuthor.style.color = styleForm.authorTagColor.value;
-		}
-	}
-	else
-	{
-		tempAuthor.style.color = styleForm.authorTagColor.value;
-	}
-	if ((styleForm.size.value <= 0) && (styleForm.size.value > 36))
-	{
-		alert("IDK, it's not Wix.com. Your font size doesn't exist in the select limitations");
-		tempSize = "12pt"
-	}
-	else
-	{
-		tempSize = styleForm.size.value + "pt";
-	}
-	tempAuthor.style.fontSize = tempSize; //Nope we're not messing with shorthand in the backend
-	if (styleForm.authorTagCustomization.value == "italics")
-	{
-		tempAuthor.style.fontStyle = "italic";
-	}
-	else if (styleForm.authorTagCustomization.value == "underline")
-	{
-		tempAuthor.style.textDecoration = "underline";
-	}
-	else if (styleForm.authorTagCustomization.value == "bold")
-	{
-		tempAuthor.style.fontWeight = "bold";
-	}
-	//The none option automatically returns
-
-	let tempDescription = document.createElement("div");
-	tempDescription.textContent = cardDescription.value;
-	tempDescription.style.fontFamily = styleForm.fontFamily.value;
-	tempDescription.style.color = styleForm.descriptionTagColor.value;
-	//Sets bold italics and what not because I'm not sitting here and dealing with shorthand
-	if (styleForm.descriptionCustomization.value == "italics")
-	{
-		tempDescription.style.fontStyle = "italic";
-	}
-	else if (styleForm.descriptionCustomization.value == "underline")
-	{
-		tempDescription.style.textDecoration = "underline";
-	}
-	else if (styleForm.descriptionCustomization.value == "bold")
-	{
-		tempDescription.style.fontWeight = "bold";
-	}
-	if ((styleForm.size.value <= 0) && (styleForm.size.value > 36))
-	{
-		tempDescription.style.fontSize = "12pt"
-	}
-	else
-	{
-		tempDescription.style.fontSize = styleForm.size.value + "pt";
-	}
-
-	let tempPaste = document.createElement("div");
-	tempPaste.innerText = inputForm.pasteBox.value;
-	tempPaste.style.fontFamily = styleForm.fontFamily.value;
-	if ((styleForm.size.value <= 0) && (styleForm.size.value > 36))
-	{
-		tempPaste.style.fontSize = "12pt"
-	}
-	else
-	{
-		tempPaste.style.fontSize = styleForm.size.value + "pt";
-	}
-
-	display(tempLabel, tempAuthor, tempDescription, tempPaste, doIWipe);
-}
-async function blockfile(doIWipe) 
-{
-	//Date
-	let date = new Date();
-	
-	let tempName = document.createElement("div");
-	tempName.innerText = `${nameJS.value} ${date.getMonth() + 1}/${date.getDate()}`;
-	tempName.style.fontFamily = "Arial"; //We could literally abstract all this but it's the same line length
-	tempName.style.fontSize = "11pt";
-	tempName.style.fontWeight = "bold";
-	tempName.style.textDecoration = "underline";
-	tempName.appendChild(document.createElement("br"));
-	tempName.appendChild(document.createElement("br"));
-
-	let tempAuthor = document.createElement("a");
-	tempAuthor.href = cardLink.value;
-	tempAuthor.style.fontFamily = "Arial";
-	tempAuthor.style.fontSize = "11pt";
-	tempAuthor.style.textDecoration = "underline";
-	tempAuthor.style.textDecorationColor = "cornflowerblue";
-	tempAuthor.style.color = "cornflowerblue";
-	tempAuthor.innerText = cardLink.value;
-	tempAuthor.appendChild(document.createElement("br"));
-
-	let tempDescription = document.createElement("div");
-	tempDescription.innerText = cardDescription.value;
-	tempDescription.style.fontFamily = "Arial";
-	tempDescription.style.fontSize = "11pt";
-
-	let tempPaste = document.createElement("div");
-	//tempPaste.innerText = inputForm.pasteBox.value;
-	tempPaste.style.fontFamily = "Arial";
-	tempPaste.style.fontSize = "11pt";
-
-	//HTML DOM Mental Gymnastics because you can't trust people not to mess with SQL Injection Hacks lmao
-	var createHighlight = function(text, isHighlighted) {
-		let tempThingy = document.createElement("span");
-		tempThingy.innerText = text;
-		tempThingy.style.fontFamily = "Arial";
-		tempThingy.style.fontSize = "11pt";
-
-		if (isHighlighted)
-		{
-			tempThingy.style.backgroundColor = `rgba(33, 252, 88, 0.8)`;
-			tempThingy.style.fontWeight = "bold";
-		}
-		tempPaste.appendChild(tempThingy);
-	}
-
-	let pasteArray = inputForm.pasteBox.value.split("\\");
-
-	//Due to some wack math nature of sandwiched arrays, only the odd intervals will need to be highlighted
-	//IDK, ask someone in that math team class because software engineers hate math with a passion
-	//That's why computers were invented
-
-	var tempBoolean = false;
-	for (var c=0; c<pasteArray.length; c++)
-	{
-		createHighlight(pasteArray[c], tempBoolean);
-		tempBoolean = !tempBoolean;
-	}
-	
-	tempPaste.appendChild(document.createElement("br"));
-	tempPaste.appendChild(document.createElement("br"));
-	styleForm.descriptionPosition.value = "after";
-
-	display(tempName, tempAuthor, tempDescription, tempPaste, doIWipe);
-}
-function display(tempLabel, tempAuthor, tempDescription, tempPaste, doIWipe) //Literally just copy this
-{
-	outputDiv.appendChild(tempLabel);
-	outputDiv.appendChild(tempAuthor);
-
-	if (styleForm.descriptionPosition.value == "before")
-	{
-		outputDiv.appendChild(tempDescription);
-		outputDiv.appendChild(tempPaste);
-	}
-	else
-	{
-		outputDiv.appendChild(tempPaste);
-		outputDiv.appendChild(tempDescription);
-	}
-
-	if (doIWipe)
-	{
-		nameJS.value = "";
-		authorTag.value = "";
-		cardLabel.value = "";
-		cardDescription.value = "";
-		cardLink.value = "";
-		inputForm.pasteBox.value = "";
-	}
-
-	document.querySelector("section").style.height = (document.body.scrollHeight + 50) + "px";	
-}
-function asyncQuerySwap(htmlElement) //Swap the inputs
-{
-	if (swappedBoolean) //This will always run when blockfile is switched out
-	{
-		nameJS.style.display = "none";
-		nameOut.style.display = "none";
-		cardLabel.style.display = "inline-block";
-		cardLabelBoxOut.style.display = "inline-block";
-
-		inputForm.children[0].placeholder = "Your paste goes here!"
-
-		swappedBoolean = false;
-	}
-	else
-	{
-		if (htmlElement.presets.value == "blockfile") //maybe add some animations
-		{
-			nameJS.style.display = "inline-block";
-			nameOut.style.display = "inline-block";
-			cardLabel.style.display = "none";
-			cardLabelBoxOut.style.display = "none";
-
-			inputForm.children[0].placeholder = "Your paste goes here! Use \\ around what you want to highlight!"
-			
-			swappedBoolean = true;
-		}
-	}
+		tempWrap.classList.remove("fadeBlackOut");
+		tempWrap.style.visibility = "hidden";
+	}, 1000);
 }
--- Script/navControl.js
+++ Script/navControl.js
@@ -1,3 +1,6 @@
+const gitJustin = 'https://github.com/ComradeDiamond';
+const mailToJustin = 'mailto:JustinChen3946@gmail.com';
+
 const editNav = (windowY, topNav) => {
 	return windowY ? () => {
 		topNav.classList.add("active");
