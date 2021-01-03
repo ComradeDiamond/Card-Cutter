bc14856d9f62e45e6dcd827da5d29ada043f09a8
Justin <60296568+ComradeDiamond@users.noreply.github.com>
2021-01-03 12:44:06 -0500

    Finished Backend (See Description)
    - Registered the modal items in the backend
    - Created backend functions to parse the modal inputs more efficiently
    - Added function to generate custom style cards
    - Added Justin preset support
    - Paste function now works when clicking the paste image

--- Script/index.js
+++ Script/index.js
@@ -1,5 +1,5 @@
 customizations = {
-	presets: "none",
+	presets: "None",
 	font: "Times New Roman",
 	cardSize: "12pt",
 	highlightColor: "None",
@@ -18,11 +18,11 @@ customizations = {
 //to store everything in the frontend
 //Parallel array to the indexHTML order and customizations JSON
 const customizationNames = [
-	"presets", 
-	"font", 
-	"cardSize", 
-	"highlightColor", 
-	"linkColor", 
+	"presets",
+	"font",
+	"cardSize",
+	"highlightColor",
+	"linkColor",
 	"linkCustom",
 	"descriptionColor",
 	"descriptionPosition",
@@ -32,7 +32,7 @@ const customizationNames = [
 	"tagCustomization"
 ];
 
-function initialize(document)
+function initialize(document) 
 {
 	custArr = Array.from(document.getElementsByClassName("flexCard"));
 	//Adds an event listener to each array and update all of their customizations
@@ -79,10 +79,20 @@ const mdcDisplaynt = (document, tempWrap, divItem, idx) => {
 	divItem.classList.add("zoomOut");
 	divItem.classList.remove("zoomIn");
 
-	let customizationVal = divItem.querySelector(".modalSelect").value;
+	//Determines the values + account for stuff if nuances exist
+	let addItemDiv = divItem.querySelector(".addItem");
+	let finalCustVal;
 
-	customizations[customizationNames[idx]] = customizationVal;
-	custArr[idx].querySelector(".flexVal").innerText = customizationVal;
+	if (addItemDiv && addItemDiv.value != "") {
+		finalCustVal = addItemDiv.value;
+	}
+	else {
+		let customizationVal = divItem.querySelector(".modalSelect").value;
+		finalCustVal = divItem.classList.contains("fontNum") ? customizationVal + "pt" : customizationVal;
+	}
+
+	customizations[customizationNames[idx]] = finalCustVal;
+	custArr[idx].querySelector(".flexVal").innerText = finalCustVal;
 
 	//Async clear elements and stuff after it ends
 	setTimeout(() => {
@@ -94,4 +104,118 @@ const mdcDisplaynt = (document, tempWrap, divItem, idx) => {
 		tempWrap.classList.remove("fadeBlackOut");
 		tempWrap.style.visibility = "hidden";
 	}, 1000);
+}
+
+//Pure func that generates the card along with the CSS
+const genCard = (inputForm, customizations) => {
+	let dom = document.createElement("div");
+	dom.style.fontSize = customizations.cardSize;
+	dom.style.fontFamily = customizations.font;
+
+	let tag = document.createElement("div");
+	tag.innerText = inputForm.tag.value;
+	tag.style.fontSize = customizations.tagSize;
+	tag.style.color = customizations.tagColor;
+	tag.style.textAlign = customizations.tagAlign;
+	applyCustomization(tag, customizations.tagCustomization);
+
+	let link = document.createElement("div");
+	link.innerText = inputForm.author.value;
+	link.addEventListener("click", () => {
+		window.open(inputForm.link.value, "_blank");
+	});
+	link.style.color = customizations.linkColor;
+	applyCustomization(link, customizations.linkCustom);
+
+	let text = document.createElement("div");
+	//Offtrack - calculating the text and highlights
+	//Since we split the items via \, only array idxes to the right of \ will need to be highlighted
+	//Hence, only odd idxes need to get hlEl before adding to the cumulation variable
+	var hlEl = `<mark style="background-color: ${customizations.highlightColor};">`;
+	var txt = inputForm.content.value.split("\\").reduce((cum, str, idx) =>
+		cum + (idx % 2 == 0 ? str : hlEl + str + `</mark>`)
+		, "");
+	//Resume track
+	text.innerHTML = txt;
+
+	let description = document.createElement("div");
+	description.appendChild(document.createElement("br"));
+	description.innerText = inputForm.desc.value;
+	description.appendChild(document.createElement("br"));
+	description.style.color = customizations.descriptionColor;
+
+	dom.appendChild(tag);
+	dom.appendChild(link);
+
+	//Accounts for description position
+	if (customizations.descriptionPosition == "Before") {
+		dom.appendChild(description);
+		dom.appendChild(text);
+	}
+	else {
+		dom.appendChild(text);
+		dom.appendChild(description);
+	}
+
+	return dom;
+}
+
+//Applies necessary customizations
+const applyCustomization = (element, customizations) => {
+	if (customizations.includes("Bold")) {
+		element.style.fontWeight = "bold";
+	}
+
+	if (customizations.includes("Italics")) {
+		element.style.fontStyle = "italic";
+	}
+
+	if (customizations.includes("Underline")) {
+		element.style.textDecorationLine = "underline";
+	}
+}
+
+const displayCard = (inputForm, cardItem, customizations) => {
+	fetchPreset(customizations)
+		.then(json => {
+			console.log("I resolved!")
+			let dom = genCard(inputForm, json);
+
+			cardItem.innerHTML = "";
+			cardItem.appendChild(dom);
+		});
+}
+
+//Fetches the preset necessary to style the card
+//Not sure if I should call this pure
+function fetchPreset(customizations) 
+{
+	return new Promise((resolve, reject) => {
+		const item = customizations.presets;
+
+		if (item == "None") {
+			resolve(customizations);
+			return;
+		}
+
+		//Fetch the JSON bc I don't want to make new vars lmao
+		let presetReq = fetch(`./Script/Presets/${item}.json`, {
+			headers: {
+				'Content-Type': 'application/json',
+				'Accept': 'application/json'
+			}
+		});
+
+		//If something wack went on with fetching, then just use the normal preset
+		presetReq.catch(err => {
+			console.error(err);
+			console.log("Most likely, some person was messing around with the input vals >:(");
+			resolve(customizations);
+		})
+
+		presetReq.then(resp => resp.json())
+			.then(json => {
+				resolve(json);
+			})
+	});
 }
